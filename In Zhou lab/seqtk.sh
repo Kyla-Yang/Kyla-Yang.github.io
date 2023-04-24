19344b7290a391d3eed99719ff0278f718ba5614
Kyla-Yang <101692246+Kyla-Yang@users.noreply.github.com>
2023-04-24 21:27:40 +0800

    Update seqtk.sh

--- In Zhou lab/seqtk.sh
+++ In Zhou lab/seqtk.sh
@@ -5,8 +5,10 @@ $samtools quickcheck M1 sorted.bam
 #抽取小部分reads进行运行和检验
 seqtk sample -s 123 F1_1.fastq.gz 100 > r1_sample.fastq
 seqtk sample -s 123 F1_2.fastq.gz 100 > r2_sample.fastq
-bwa mem $genome $r1_sample.fastq $r2_sample.fastq > $sample.sam
-samtools view -S -b $sample.sam > $sample.bam
-samtools sort $sample.bam -o $sample_sorted.bam
-samtools index $sample_sorted.bam
-rm $sample.sam
+r1_sample=r1_sample.fastq
+r2_sample=r2_sample.fastq
+bwa mem $genome $r1_sample.fastq $r2_sample.fastq > /public2/home/rotation/yyn/He_reseq/Here.Female/sample.sam
+samtools view -S -b sample.sam > sample.bam
+samtools sort sample.bam -o sample_sorted.bam
+samtools index sample_sorted.bam
+rm sample.sam
