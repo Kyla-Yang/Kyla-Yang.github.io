7f43505422fc782ef0d63ccd89f17b96b8cd5a5f
Kyla-Yang <101692246+Kyla-Yang@users.noreply.github.com>
2023-04-24 19:22:32 +0800

    Update gatk.sh

--- In Zhou lab/gatk.sh
+++ In Zhou lab/gatk.sh
@@ -1,18 +1,19 @@
 conda activate gatk_env
 
-for f in /public2/home/rotation/yyn/He_reseq/sorted_bam/Here.Female *_sorted.bam
+for f in /public2/home/rotation/yyn/He_reseq/sorted_bam/Here.male *_sorted.bam
 do
    echo "Processing $f file..."
    prefix=$(basename $f _sorted.bam)
    sorted_rmdup_bam=${prefix}_sorted_rmdup.bam
-   gatk HaplotypeCaller -R /public2/home/rotation/yyn/He_genome.fasta -I $f --genotyping-mode DISCOVERY -stand-call-conf 20 --emit-ref-confidence GVCF -O ${prefix}.g.vcf.gz
-   gatk CombineGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.g.vcf.gz --output ${prefix}.g.vcf.gz
-   gatk GenotypeGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.g.vcf.gz --output ${prefix}.raw.vcf.gz
-   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.raw.vcf.gz --output ${prefix}.raw.snp.vcf.gz
-   gatk VariantFiltration --variant ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" --output ${prefix}.flag.snp.vcf.gz
-   gatk SelectVariants --exclude-filtered true --restrict-alleles-to BIALLELIC --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.flag.snp.vcf.gz --output ${prefix}.hardfiltered.snp.vcf.gz
-   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.raw.vcf.gz --output ${prefix}.raw.snp.vcf.gz
-   gatk SelectVariants --select-type-to-include INDEL --reference /public2/home/rotation/yyn/He_genome.fasta --variant ${prefix}.raw.vcf.gz --output ${prefix}.raw.indel.vcf.gz
+   gatk HaplotypeCaller -R /public2/home/rotation/yyn/He_genome.fasta -I $f -ERC GVCF --emit-ref-confidence GVCF -O ${prefix}_g.vcf.gz
+   gatk CombineGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}_g.vcf.gz -O ${prefix}.g.vcf.gz
+   gatk GenotypeGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.g.vcf.gz -O ${prefix}.raw.vcf.gz
+   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.snp.vcf.gz
+   gatk VariantFiltration -V ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" -O ${prefix}.flag.snp.vcf.gz
+   gatk SelectVariants --exclude-filtered true --restrict-alleles-to BIALLELIC --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.flag.snp.vcf.gz -O ${prefix}.hardfiltered.snp.vcf.gz
+   gatk SelectVariants --select-type-to-include INDEL --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.indel.vcf.gz
+   gatk VariantFiltration -V ${prefix}.raw.indel.vcf.gz -O ${prefix}.filtered.indel.vcf.gz --filter-expression "QD < 2.0 ||FS > 200.0 || MQ < 40.0 || SOR > 10.0 || ReadPosRankSum < -8.0"
+   gatk SelectVariants --exclude-filtered -V ${prefix}.filtered.indel.vcf.gz -O ${prefix}.filtered.indel.pass.vcf.gz 
 done
 
 conda deactivate
