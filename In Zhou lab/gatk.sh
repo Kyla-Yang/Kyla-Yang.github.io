aa4afd1281cbf5122226471d5921f9e37b05167e
Kyla-Yang <101692246+Kyla-Yang@users.noreply.github.com>
2023-06-18 14:00:43 +0800

    Update gatk.sh

--- In Zhou lab/gatk.sh
+++ In Zhou lab/gatk.sh
@@ -6,10 +6,10 @@ do
    prefix=$(basename $f _sorted.bam)
    sorted_rmdup_bam=${prefix}_sorted_rmdup.bam
    gatk HaplotypeCaller --native-pair-hmm-threads 4 -R /public2/home/rotation/yyn/He_genome.fasta -I $f -ERC GVCF --emit-ref-confidence GVCF -O ${prefix}_g.vcf.gz
-   gatk CombineGVCFs --native-pair-hmm-threads 4 --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}_g.vcf.gz -O ${prefix}.g.vcf.gz
-   gatk GenotypeGVCFs --native-pair-hmm-threads 8 --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.g.vcf.gz -O ${prefix}.raw.vcf.gz
-   gatk SelectVariants --native-pair-hmm-threads 4 --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.snp.vcf.gz
-   gatk VariantFiltration --native-pair-hmm-threads 4 -V ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" -O ${prefix}.flag.snp.vcf.gz
+   gatk CombineGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}_g.vcf.gz -O ${prefix}.g.vcf.gz
+   gatk GenotypeGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.g.vcf.gz -O ${prefix}.raw.vcf.gz
+   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.snp.vcf.gz
+   gatk VariantFiltration -V ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" -O ${prefix}.flag.snp.vcf.gz
    gatk SelectVariants --native-pair-hmm-threads 4 --exclude-filtered true --restrict-alleles-to BIALLELIC --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.flag.snp.vcf.gz -O ${prefix}.hardfiltered.snp.vcf.gz
 done
 
