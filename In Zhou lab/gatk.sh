8834de0df620d8f6c437548945950866b42d8dfe
Kyla-Yang <101692246+Kyla-Yang@users.noreply.github.com>
2023-06-20 20:16:40 +0800

    Update gatk.sh

--- In Zhou lab/gatk.sh
+++ In Zhou lab/gatk.sh
@@ -5,14 +5,21 @@ do
    echo "Processing $f file..."
    prefix=$(basename $f _sorted_rmdup_addrg.bam)
    gatk HaplotypeCaller --native-pair-hmm-threads 4 -R /public2/home/rotation/yyn/He_genome.fasta -I $f -ERC GVCF --emit-ref-confidence GVCF -O ${prefix}_g.vcf.gz
-   gatk CombineGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}_g.vcf.gz -O ${prefix}.g.vcf.gz --spark-master local[4]
-   gatk GenotypeGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.g.vcf.gz -O ${prefix}.raw.vcf.gz --spark-master local[8]
-   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.snp.vcf.gz --spark-master local[4]
-   gatk VariantFiltration -V ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" -O ${prefix}.flag.snp.vcf.gz --spark-master local[4]
-   gatk SelectVariants --native-pair-hmm-threads 4 --exclude-filtered true --restrict-alleles-to BIALLELIC --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.flag.snp.vcf.gz -O ${prefix}.hardfiltered.snp.vcf.gz --spark-master local[4]
+   gatk CombineGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}_g.vcf.gz -O ${prefix}.g.vcf.gz
+   gatk GenotypeGVCFs --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.g.vcf.gz -O ${prefix}.raw.vcf.gz
+   gatk SelectVariants --select-type-to-include SNP --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.snp.vcf.gz
+   gatk VariantFiltration -V ${prefix}.raw.snp.vcf.gz --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "snp_filter" --genotype-filter-expression "DP < 2 || DP > 50" --genotype-filter-name "dp_fail" -O ${prefix}.flag.snp.vcf.gz
+   gatk SelectVariants --native-pair-hmm-threads 4 --exclude-filtered true --restrict-alleles-to BIALLELIC --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.flag.snp.vcf.gz -O ${prefix}.hardfiltered.snp.vcf.gz
 done
-
 conda deactivate
+
+#CombineGVCFs with 4 codes
+#GenotypeGVCFs with 4-8 codes
+#SelectVariants with 4-8 codes
+#VariantFiltration with 4-8 codes
+#Therotically, '--saprk-master local [4]' can be used to invoke multiple codes, but it appears an error in my project: 
+#A USER ERROR has occurred: spark-master is not a recognized option.
+
 
 #Indel
 gatk SelectVariants --select-type-to-include INDEL --reference /public2/home/rotation/yyn/He_genome.fasta -V ${prefix}.raw.vcf.gz -O ${prefix}.raw.indel.vcf.gz
